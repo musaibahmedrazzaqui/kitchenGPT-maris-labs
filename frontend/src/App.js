6527d9fcd60dede7e57ee72ea03d170cb109057f
Musaib Ahmed Razzaqui <musaibahmed2000@gmail.com>
2023-11-27 21:23:45 +0500

    new ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,144 @@
-import React from "react";
-import Chatbot from "./IngredientForm";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  MainContainer,
+  MessageInput,
+  MessageHeader,
+  MessageList,
+  MessageContainer
+} from "@minchat/react-chat-ui";
 
-function App() {
+
+
+const App = () => {
+  const [collectedMessages, setMessages] = useState([{
+    text: 'Hello',
+    user: {
+        id: 'assistant',
+        name: 'Assistant',
+    },
+}]);
+  const [inputMessage, setInputMessage] = useState('');
+  const [chatMode, setChatMode] = useState('normal');
+
+  useEffect(() => {
+    if (inputMessage.trim() !== '') {
+      fetchMessages();
+    }
+  }, [chatMode]);
+
+  const fetchMessages = async (text) => {
+    try {
+      const apiUrl =
+        chatMode === 'dropout'
+          ? 'https://backend-finetune-gpt.vercel.app/api/product/chatbot-dropout'
+          : 'https://backend-finetune-gpt.vercel.app/api/product/chatbot-normal';
+      const newMessageUser = {
+            text: text,
+            user: {
+              id: 'dan',
+              name: 'Dan',
+            },
+          };
+          //setMessages([...collectedMessages, newMessageUser])
+      const response = await axios.post(apiUrl, { message: text });
+      console.log('Response',response)
+      
+      const newMessage = {
+        text: response.data.response,
+        user: {
+          id: 'assistant',
+          name: 'Assistant',
+        },
+      };
+
+      setMessages([...collectedMessages,newMessageUser, newMessage]);
+      setInputMessage('');
+    } catch (error) {
+      console.error('Error fetching collectedMessages:', error);
+      const errorMessage = 'Error occurred while processing your request.';
+      const errorResponse = {
+        text: errorMessage,
+        user: {
+          id: 'assistant',
+          name: 'Assistant',
+        },
+      };
+      setMessages([...collectedMessages, errorResponse]);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    console.log("handleinput change")
+    setInputMessage(e.target);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = (text) => {
+    setInputMessage(text)
+    console.log("inside handle messages",text)
+    if (text.trim() !== '') {
+      console.log("inside if")
+      fetchMessages(text);
+    }
+  };
+
+  const handleModeChange = (e) => {
+    setChatMode(e.target.value);
+    setMessages([]);
+  };
+
   return (
-    <div className="App">
-      <h1 style={{fontSize:'35px',fontStyle:"bold",margin:'auto',marginLeft:'22%',alignItems:'center',justifyContent:'center',alignSelf:'center'}}>Pharmaceutical Drug Review Chatbot</h1>
-      {/* <h3 style={{fontSize:'15px',margin:'auto',marginLeft:'15px',alignItems:'center',justifyContent:'center',alignSelf:'center',color:"white"}}>PHARMACEUTICAL DRUG REVIEW CHATBOT</h3> */}
-      <Chatbot />
-      <h3 style={{fontSize:'15px',margin:'auto',marginLeft:'15px',alignItems:'center',justifyContent:'center',alignSelf:'center',color:"white"}}>For research purposes. The chatbot is made from a dataset for research purposes only to study the effect of incororporating user-generated data while training chatbots.</h3>
-    </div>
+    <MainContainer style={{ height: '100vh' }}>
+      
+      <MessageContainer>
+      <div className="radio-container" style={{backgroundColor:'#F3F4F6',borderRadius:'20px',alignItems:'center',justifyContent:'center',textAlign:'center',paddingTop:'10px'}}>
+        <h3>PHARMACEUTICAL DRUG REVIEW CHATBOT</h3>
+        <input
+          type="radio"
+          id="normal"
+          name="chatMode"
+          value="normal"
+          checked={chatMode === 'normal'}
+          onChange={handleModeChange}
+        />
+        <label htmlFor="normal">Normal</label>
+
+        <input
+          type="radio"
+          id="dropout"
+          name="chatMode"
+          value="dropout"
+          checked={chatMode === 'dropout'}
+          onChange={handleModeChange}
+        />
+        <label htmlFor="dropout">Dropout</label>
+        <h5 style={{ fontSize: '12px', textAlign: 'center' }}>
+        Use a prompt like this: "My age group is AGE_GROUP. Tell me about a user's experience with DRUG_NAME"
+      </h5>
+      </div>
+    
+        <MessageHeader />
+        <MessageList
+          currentUserId='dan'
+          messages={collectedMessages}
+        />
+        {console.log(collectedMessages)}
+        <MessageInput
+          placeholder="Type a message..."
+          onSendMessage={handleSendMessage}
+         
+          value={inputMessage}
+        />
+        {/* <h5 style={{ fontSize: '12px' }}>Use a prompt like this:"My age group is AGE_GROUP. Tell me about a user's experience with DRUG_NAME"</h5> */}
+      </MessageContainer>
+    </MainContainer>
   );
-}
+};
 
 export default App;
